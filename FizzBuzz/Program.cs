cd3111bb335f9fb43e611a0e9c791e1a910f6268
almightylks <lukasbarber@web.de>
2020-10-08 19:44:35 +0200

    init

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i < 39; i++)
+            {
+                System.Console.WriteLine(FizzBuzz(i));
+                System.Threading.Thread.Sleep(50);
+            }
+        }
+        static string FizzBuzz(int num)
+        {
+            return num % 3 == 0
+                ? (num % 5 == 0 ? "FizzBuzz" : "Fizz")
+                : num % 5 == 0 ? "Buzz" : num.ToString();
+        }
+    }
+}
